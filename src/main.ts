d13fe214b250fb801c15c2ca2fd73dd4774fd9d7
nimamleo <nimamahini81@gmail.com>
2024-10-14 22:10:17 +0330

    ci

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const logger = new Logger('app');
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('Your API description')
+    .setVersion('1.0')
+    .addTag('SHOP')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3010);
+  logger.verbose(`app is running on port ${3010}`);
 }
 bootstrap();
