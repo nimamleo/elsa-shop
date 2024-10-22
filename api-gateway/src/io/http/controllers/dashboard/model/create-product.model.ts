468461aa5eae9fff10cadb80239a2f9d3c8dbae0
nimamleo <nimamahini81@gmail.com>
2024-10-22 09:28:37 +0330

    add info

--- api-gateway/src/io/http/controllers/dashboard/model/create-product.model.ts
+++ api-gateway/src/io/http/controllers/dashboard/model/create-product.model.ts
@@ -61,7 +61,7 @@ export class CreateProductRequest {
   categoryId: string;
 
   @ApiProperty({ type: [CreateProductInfo] })
-  @IsArray({ each: true })
+  @IsArray()
   info: CreateProductInfo[];
 }
 
--- product/src/application/product/database/pgsql/service/product-pgsql.service.ts
+++ product/src/application/product/database/pgsql/service/product-pgsql.service.ts
@@ -2,12 +2,14 @@ import { IProductDatabaseProvider } from '../../provider/product.provider';
 import { IProduct, IProductEntity } from '../../../models/product.model';
 import { Err, Ok, Result } from '@common/result';
 import { HandleError } from '@common/decorators/handle-error.decorator';
-import { InjectRepository } from '@nestjs/typeorm';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
 import { ProductEntity } from '../entities/product.entity';
-import { Repository } from 'typeorm';
+import { DataSource, EntityManager, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { ICategory, ICategoryEntity } from '../../../models/category.model';
 import { CategoryEntity } from '../entities/category.entity';
+import { Product1729446557373 } from '@infrastructure/infrastructure/database/pgsql/migrations/product/1729446557373-product';
+import { InfoEntity } from '../entities/info.entity';
 
 @Injectable()
 export class ProductPgsqlService implements IProductDatabaseProvider {
@@ -16,16 +18,38 @@ export class ProductPgsqlService implements IProductDatabaseProvider {
     private readonly productRepository: Repository<ProductEntity>,
     @InjectRepository(CategoryEntity)
     private readonly categoryRepository: Repository<CategoryEntity>,
+    @InjectRepository(InfoEntity)
+    private readonly infoRepository: Repository<InfoEntity>,
+    @InjectDataSource()
+    private readonly dataSource: DataSource,
   ) {}
 
   @HandleError
   async createProduct(iProduct: IProduct): Promise<Result<IProductEntity>> {
-    const res = await this.productRepository.save(
-      ProductEntity.fromIProduct(iProduct),
+    const res = await this.dataSource.transaction(
+      async (entityManager: EntityManager): Promise<ProductEntity> => {
+        const product = await entityManager
+          .getRepository(ProductEntity)
+          .save(ProductEntity.fromIProduct(iProduct));
+
+        product.info.map((x) =>
+          InfoEntity.fromIInfo({
+            product: { id: product.id.toString() },
+            count: x.count,
+            size: x.size,
+            color: x.color,
+          }),
+        );
+
+        const info = await entityManager
+          .getRepository(InfoEntity)
+          .save(product.info);
+
+        product.info = info;
+
+        return product;
+      },
     );
-    if (!res) {
-      return Err('create product failed');
-    }
 
     return Ok(ProductEntity.toIProductEntity(res));
   }
