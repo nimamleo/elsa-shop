b71d06c04b6e47247a6c885c3e777f2e40d25fcf
nimamleo <nima.mahini@g2holding.org>
2024-10-27 18:09:11 +0330

    connect to bucket

--- asset/src/application/asset/storage/S3/storage-s3.service.ts
+++ asset/src/application/asset/storage/S3/storage-s3.service.ts
@@ -1,10 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { IStorageProvider } from '../provider/storage.provider';
 import { Result } from '@common/result';
+import {
+  IAssetProvider,
+  PARSPACK_BUCKET_TOKEN,
+} from '@infrastructure/infrastructure/asset/providers/asset.provider';
+import { HandleError } from '@common/decorators/handle-error.decorator';
+import { CreateMultipartUploadCommand } from '@aws-sdk/client-s3';
 
 @Injectable()
 export class StorageS3Service implements IStorageProvider {
-  uploadFile(): Promise<Result<boolean>> {}
+  constructor(
+    @Inject(PARSPACK_BUCKET_TOKEN)
+    private readonly storageProvider: IAssetProvider,
+  ) {}
+
+  @HandleError
+  async uploadFile(): Promise<Result<boolean>> {
+    const createCommand = new CreateMultipartUploadCommand({});
+    // const result = await this.storageProvider.getS3CLinet().;
+  }
   serverFile(): Promise<Result<boolean>> {}
   deleteFile(): Promise<Result<boolean>> {}
 }
