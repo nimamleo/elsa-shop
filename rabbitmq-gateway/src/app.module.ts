9e8493b27ca582298bd194a52ee57f5283e6ac00
nimamleo <nimamahini81@gmail.com>
2024-11-12 01:12:06 +0330

    rabbitmq docker

--- rabbitmq-gateway/src/app.module.ts
+++ rabbitmq-gateway/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { IoModule } from './io/io.module';
+import { ConfigModule } from '@nestjs/config';
+import { kavenegarConfig } from '@sms/application/sms/kavenegar/config/kavenegar.config';
 
 @Module({
-  imports: [IoModule],
+  imports: [
+    IoModule,
+    ConfigModule.forRoot({
+      load: [kavenegarConfig],
+      cache: true,
+      envFilePath: ['.env'],
+    }),
+  ],
 })
 export class AppModule {}
--- rabbitmq-gateway/src/io/io.module.ts
+++ rabbitmq-gateway/src/io/io.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SendSmsRabbitmqController } from './rabbitmq/send-sms-rabbitmq.controller';
+import { SmsModule } from '@sms/application/sms/sms.module';
 
 @Module({
+  imports: [SmsModule],
   controllers: [SendSmsRabbitmqController],
 })
 export class IoModule {}
